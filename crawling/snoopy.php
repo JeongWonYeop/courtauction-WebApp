42450e561405a70417784f3fdb64696755282c09
정원엽 <nerbs0313@gmail.com>
2020-05-22 07:56:33 +0900

    개발자 모드에서조차도 안보이는 min 뒤의 숨은 공백 있는지 브라우저 상에서 한번 확인 하는 작업. 이 숨은 공백이 보안적인 역할을 하기 때문에 찾아 내는게 중요하다. "min"> 인경우가 있고 "min" > 인경우가 있다.

--- crawling/snoopy.php
+++ crawling/snoopy.php
@@ -16,14 +16,8 @@ $o="";
 $snoopy->fetch("http://www.landfuture.co.kr/workdir/upcate/kyg/kyg_srch.php?s_year=".$x[0]."&s_sa_num=".$x[1]);
 //스누피의 fetch함수로 제 웹페이지를 긁어볼까요? :)
 $result=$snoopy->results;
-//$rex="/								(.*)/";
-// $rex="/(.*)					/";
-$rex="/\"address\" \>\n								(.*)					/"; 
-
-//놀라운점 하나. address뒤에 공백이 하나 숨어 있었다.
-//놀라운점 둘. (.*)은 줄바꿈 이전까지만 뽑아준다. 즉, 줄바꿈이 있는 경우에는 그 줄바꿈까지 정규식에 넣어주어야 비로소 텍스트를 뽑아준다.
-// $rex="/559		+(.*)/";
-// $rex="/								(.*)					/";
+//$addressrex="/\"address\" \>\n								(.*)					/"; 
+$rex="/\"min\"(.*)>/";
 preg_match_all($rex,iconv("euc-kr","utf-8",$result), $text);
 
 
