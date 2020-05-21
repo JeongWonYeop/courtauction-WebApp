d0a4520eab7b34d303d17c13dc448a3f72f007e1
정원엽 <nerbs0313@gmail.com>
2020-05-22 07:42:53 +0900

    브라우저에 표시되는 값이랑, 개발자 모드에서 보이는 것이 다르다.

--- crawling/snoopy.php
+++ crawling/snoopy.php
@@ -1,7 +1,7 @@
 <?php
 
 //Snoopy.class.php를 불러옵니다
-$x=array("2017","1310");
+$x=array("2017","1101");
 require($_SERVER['DOCUMENT_ROOT'].'/Snoopy-2.0.0.tar.gz/Snoopy.class.php');
 
  
@@ -18,14 +18,14 @@ $snoopy->fetch("http://www.landfuture.co.kr/workdir/upcate/kyg/kyg_srch.php?s_ye
 $result=$snoopy->results;
 //$rex="/								(.*)/";
 // $rex="/(.*)					/";
-$rex="/\"address\" \>\n(.*)/";
+$rex="/\"address\" \>\n								(.*)					/"; 
 
 //놀라운점 하나. address뒤에 공백이 하나 숨어 있었다.
 //놀라운점 둘. (.*)은 줄바꿈 이전까지만 뽑아준다. 즉, 줄바꿈이 있는 경우에는 그 줄바꿈까지 정규식에 넣어주어야 비로소 텍스트를 뽑아준다.
 // $rex="/559		+(.*)/";
 // $rex="/								(.*)					/";
 preg_match_all($rex,iconv("euc-kr","utf-8",$result), $text);
-str_replace('address','',$text[0][0]);
+
 
 print_r($text);
 $convsnoopy =iconv("euc-kr","utf-8",$result); 
