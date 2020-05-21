fb83dc77b737de0ce92d7c2afcd71d3121e5b699
정원엽 <nerbs0313@gmail.com>
2020-05-22 07:15:33 +0900

    crawling code update

--- crawling/snoopy.php
+++ crawling/snoopy.php
@@ -18,12 +18,14 @@ $snoopy->fetch("http://www.landfuture.co.kr/workdir/upcate/kyg/kyg_srch.php?s_ye
 $result=$snoopy->results;
 //$rex="/								(.*)/";
 // $rex="/(.*)					/";
-$rex="/\"address\" \>(\r?\n)(.*)/"; 
+$rex="/\"address\" \>\n(.*)/";
+
 //놀라운점 하나. address뒤에 공백이 하나 숨어 있었다.
 //놀라운점 둘. (.*)은 줄바꿈 이전까지만 뽑아준다. 즉, 줄바꿈이 있는 경우에는 그 줄바꿈까지 정규식에 넣어주어야 비로소 텍스트를 뽑아준다.
 // $rex="/559		+(.*)/";
 // $rex="/								(.*)					/";
 preg_match_all($rex,iconv("euc-kr","utf-8",$result), $text);
+str_replace('address','',$text[0][0]);
 
 print_r($text);
 $convsnoopy =iconv("euc-kr","utf-8",$result); 
